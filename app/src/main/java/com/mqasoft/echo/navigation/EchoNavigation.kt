9157281225c08b4fc557b7c34f7af3a2d09f870e
Abdul Moeed <36700038+abdulmoeed11@users.noreply.github.com>
2024-03-05 15:12:58 +0300

    Revert "Feature chatting"

--- app/src/main/java/com/mqasoft/echo/navigation/EchoNavigation.kt
+++ app/src/main/java/com/mqasoft/echo/navigation/EchoNavigation.kt
@@ -5,27 +5,19 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.mqasoft.echo.core.navigation.Screen
-import com.mqasoft.echo.login.navigation.addLoginNestedGraph
 import com.mqasoft.echo.login.presentation.login.LoginScreen
 import com.mqasoft.echo.welcome.WelcomeScreen
 
 @Composable
-fun EchoNavigation(loggedIn: Boolean){
+fun EchoNavigation(){
     val navController = rememberNavController()
-    lateinit var startDestination: String
-    if (loggedIn){
-        startDestination = Screen.HomeScreen.route
-    } else{
-        startDestination = Screen.WelcomeScreen.route
-    }
-    NavHost(navController = navController, startDestination = startDestination){
+    NavHost(navController = navController, startDestination = Screen.WelcomeScreen.route){
         composable(route = Screen.WelcomeScreen.route){
             WelcomeScreen(navController)
         }
         composable(route = Screen.LoginScreen.route){
-            LoginScreen(navController = navController)
+            LoginScreen()
         }
-        addLoginNestedGraph(navController)
     }
 
 }
